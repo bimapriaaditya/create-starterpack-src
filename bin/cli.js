649d9c82e3a8c738d0ccae9b55087ee372f8f9a6
BimaPriaAditya <bpabima@gmail.com>
2024-02-06 10:23:59 +0700

    change reponame

--- bin/cli.js
+++ bin/cli.js
@@ -13,7 +13,7 @@ const runCommand = command => {
 }
 
 const repoName = process.argv[2];
-const gitCheckoutCommand = `git clone --depth 1 https://github.com/bimapriaaditya/starterpack-src ${repoName}`;
+const gitCheckoutCommand = `git clone --depth 1 https://github.com/bimapriaaditya/created-starterpack-src ${repoName}`;
 const installDepsCommand = `cd ${repoName} && npm install`;
 
 console.log(`Cloning the repository with name ${repoName}`);
