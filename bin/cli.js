44b49be80b6241af3c148033d8707ab0c10a5866
BimaPriaAditya <bpabima@gmail.com>
2024-02-06 15:15:48 +0700

    update bin cli

--- bin/cli.js
+++ bin/cli.js
@@ -1,5 +1,12 @@
 #!/usr/bin/env node
 const { execSync } = require('child_process');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
 
 const runCommand = command => {
   try {
@@ -12,17 +19,28 @@ const runCommand = command => {
   return true;
 }
 
-const repoName = process.argv[2];
-const gitCheckoutCommand = `git clone --depth 1 https://github.com/bimapriaaditya/create-starterpack-src ${repoName}`;
-const installDepsCommand = `cd ${repoName} && npm install`;
+const repoName = process.argv[2] || ".";
+const gitCheckoutCommand = `git --git-dir=/dev/null clone https://github.com/bimapriaaditya/create-starterpack-src ${repoName}`;
+const installDepsCommand = (repoName !== ".") ? `npm install` : `cd ${repoName} && npm install`;
 
 console.log(`Cloning the repository with name ${repoName}`);
 const checkedOut = runCommand(gitCheckoutCommand);
 if (!checkedOut) process.exit(-1);
 
-console.log(`Installing depedencies for ${repoName}`);
-const installDeps = runCommand(installDepsCommand);
-if (!installDeps) process.exit(-1);
+rl.question('Install javascript depedencies library right now? [Y/N]: ', (answer) => {
+  if (answer.toLowerCase() === 'y') {
+    console.log(`Installing depedencies for ${repoName}`);
+    const installDeps = runCommand(installDepsCommand);
+    if (!installDeps) process.exit(-1);
 
-console.log(`Congratulations! You are ready for hack nasa!`);
-console.log(`cd ${repoName} && npm run style`);
+  } else if (answer.toLowerCase() === 'n') {
+    console.log('Anda dapat melakukan npm install secara manual setelah clone.');
+  } else {
+    console.log('Invalid option');
+  }
+
+  rl.close();
+  
+  console.log(`Congratulations! You are ready for hack nasa!`);
+  console.log(`cd ${repoName} && npm run style`);
+});
